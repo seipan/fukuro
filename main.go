7d82b8037b2d077884bbfefdcde2ffe926be1749
seipan <yamasakipann0218@gmail.com>
2024-05-02 02:07:01 +0900

    fix : remove deplicate package

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package fukuro
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"syscall"
@@ -53,10 +52,10 @@ func InitContainer() error {
 	if err := os.MkdirAll("/sys/fs/cgroup/cpu/my-container", 0700); err != nil {
 		return fmt.Errorf("Cgroups namespace my-container create failed: %w", err)
 	}
-	if err := ioutil.WriteFile("/sys/fs/cgroup/cpu/my-container/tasks", []byte(fmt.Sprintf("%d\n", os.Getpid())), 0644); err != nil {
+	if err := os.WriteFile("/sys/fs/cgroup/cpu/my-container/tasks", []byte(fmt.Sprintf("%d\n", os.Getpid())), 0644); err != nil {
 		return fmt.Errorf("Cgroups register tasks to my-container namespace failed: %w", err)
 	}
-	if err := ioutil.WriteFile("/sys/fs/cgroup/cpu/my-container/cpu.cfs_quota_us", []byte("1000\n"), 0644); err != nil {
+	if err := os.WriteFile("/sys/fs/cgroup/cpu/my-container/cpu.cfs_quota_us", []byte("1000\n"), 0644); err != nil {
 		return fmt.Errorf("Cgroups add limit cpu.cfs_quota_us to 1000 failed: %w", err)
 	}
 
