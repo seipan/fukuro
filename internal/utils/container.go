7cfbff704e1676bcdedf6326ae9e6cf74098a851
seipan <yamasakipann0218@gmail.com>
2024-09-28 18:37:53 +0900

    feat(utils) : Add RevisePidFile

--- internal/utils/container.go
+++ internal/utils/container.go
@@ -1,1 +1,20 @@
 package utils
+
+import (
+	"path/filepath"
+
+	"github.com/urfave/cli"
+)
+
+func RevisePidFile(context *cli.Context) error {
+	pidFile := context.String("pid-file")
+	if pidFile == "" {
+		return nil
+	}
+
+	pidFile, err := filepath.Abs(pidFile)
+	if err != nil {
+		return err
+	}
+	return context.Set("pid-file", pidFile)
+}
